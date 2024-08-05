d56ebce12b14dded73b5e13103f55c921ee332ca
justinp24 <justinpardo2423@gmail.com>
2024-08-05 16:52:11 -0400

    initalize auth sentry and appwrite

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,10 +1,22 @@
 import type { Metadata } from "next";
 import { Inter, IBM_Plex_Serif } from "next/font/google";
-
+import Image from "next/image";
 export default function RootLayout({
     children,
 }: Readonly<{
     children: React.ReactNode;
 }>) {
-    return <main>{children}</main>;
+    return (
+        <main className="flex min-h-screen w-full justify-between font-inter">
+            {children}
+            <div className="auth-asset">
+                <Image
+                    src="/icons/auth-image.svg"
+                    alt="Auth"
+                    width={500}
+                    height={500}
+                />
+            </div>
+        </main>
+    );
 }
