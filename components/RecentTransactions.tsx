591cccb622237b910dfb93ea341dbb67da40d7e3
justinp24 <justinpardo2423@gmail.com>
2024-08-06 14:17:48 -0400

    view transactions based on user

--- components/RecentTransactions.tsx
+++ components/RecentTransactions.tsx
@@ -1,5 +1,9 @@
 import React from "react";
 import Link from "next/link";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { BankTabItem } from "./ui/BankTabItem";
+import BankInfo from "./ui/BankInfo";
+import TransactionsTable from "./ui/TransactionsTable";
 const RecentTransactions = ({
     accounts,
     transactions,
@@ -10,13 +14,45 @@ const RecentTransactions = ({
         <section className="recent-transactions">
             <header className="flex items-center justify-between">
                 <h2 className="recent-transactions-label">
-                    Recent Transactions
+                    Recent transactions
                 </h2>
                 <Link
                     href={`/transaction-history/?id=${appwriteItemId}`}
                     className="view-all-btn"
-                ></Link>
+                >
+                    View all
+                </Link>
             </header>
+            <Tabs defaultValue={appwriteItemId} className="w-full">
+                <TabsList className="recent-transactions-tablist">
+                    {accounts.map((account: Account) => (
+                        <TabsTrigger
+                            key={account.id}
+                            value={account.appwriteItemId}
+                        >
+                            <BankTabItem
+                                key={account.id}
+                                account={account}
+                                appwriteItemId={appwriteItemId}
+                            />
+                        </TabsTrigger>
+                    ))}
+                </TabsList>
+                {accounts.map((account: Account) => (
+                    <TabsContent
+                        value={account.appwriteItemId}
+                        key={account.id}
+                        className="space-y-4"
+                    >
+                        <BankInfo
+                            account={account}
+                            appwriteItemId={appwriteItemId}
+                            type="full"
+                        />
+                        <TransactionsTable transactions={transactions} />
+                    </TabsContent>
+                ))}
+            </Tabs>
         </section>
     );
 };
